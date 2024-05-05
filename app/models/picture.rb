99936db5f914254e74997e83de29ed3473caf600
terananay <goro2.tera.yuu@gmail.com>
2024-05-05 23:35:24 +0900

    activeStorageでのcreate, vipsを使った表示

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -7,7 +7,10 @@ class Picture < ApplicationRecord
 
   validates :is_sample, inclusion: { in: [true, false] }
   validates :shooting_date, presence: true
-  before_validation :set_shooting_date
+  attribute :shooting_date, :datetime, default: -> { Time.current }
+  # before_validation :set_shooting_date
+  validate :image_content_type
+  validate :image_size
 
   private
 
@@ -15,5 +18,17 @@ class Picture < ApplicationRecord
     return unless image.attached?
 
     self.shooting_date = EXIFR::JPEG.new(image.download).date_time_original || Time.current
+  end
+
+  def image_content_type
+    return unless image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
+
+    errors.add(:image, t('validate.image_type'))
+  end
+
+  def image_size
+    return unless image.attached? && image.blob.byte_size > 4.megabytes
+
+    errors.add(:image, t('validate.image_size'))
   end
 end
