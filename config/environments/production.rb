ffb0ff9d80d66f67ce228f8fa321e29fbf72d578
terananay <goro2.tera.yuu@gmail.com>
2024-05-07 00:35:44 +0900

    awsのバゲット情報を反映

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = :amazon_pictures
   config.active_storage.variant_processsor = :vips
 
   # Mount Action Cable outside main process or domain.
