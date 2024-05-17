bfc98e80031e140632c59071e1ad56b7ef998932
Terada_50 <145760523+terananay@users.noreply.github.com>
2024-05-17 22:02:53 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,18 +1,5 @@
 set -o errexit
 
-# exiftoolのインストール
-cd $HOME
-wget -O exiftool.tar.gz https://exiftool.org/Image-ExifTool-12.40.tar.gz
-tar -xzf exiftool.tar.gz
-cd Image-ExifTool-12.40
-perl Makefile.PL
-make
-make test
-make install
-
-# 環境変数の設定
-export PATH=$HOME/perl5/bin:$PATH
-
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
