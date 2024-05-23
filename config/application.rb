4f5b0349fd7027c3496f45c2d8fee5544acacee1
Terada_50 <145760523+terananay@users.noreply.github.com>
2024-05-23 20:33:01 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require "turbo-rails"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
